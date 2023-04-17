915de6f555ac8ec0d003506cc6a6684fa8518542
Daniel <20521181@gm.uit.edu.vn>
2023-04-17 21:37:34 +0700

    feat: make first request to Storefront API

---
+++ src/lib/shopify.js
@@ -0,0 +1,7 @@
+import { createStorefrontClient } from "@shopify/hydrogen-react";
+
+export const shopifyClient = createStorefrontClient({
+  storeDomain: `https://${process.env.PUBLIC_STORE_NAME}.myshopify.com`,
+  storefrontApiVersion: process.env.PUBLIC_STOREFRONT_API_VERSION || "2023-01",
+  privateStorefrontToken: process.env.PRIVATE_STOREFRONT_TOKEN,
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,33 @@
-export default function Home() {
+import { shopifyClient } from "../lib/shopify";
+import { GetServerSideProps } from "next";
+
+export default function Home(json: any) {
+  console.log(json);
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
+
+const GRAPHQL_QUERY = `
+  query {
+    shop {
+      name
+    }
+  }
+`;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await fetch(shopifyClient.getStorefrontApiUrl(), {
+    body: JSON.stringify({
+      query: GRAPHQL_QUERY,
+    }),
+    headers: shopifyClient.getPrivateTokenHeaders(),
+    method: "POST",
+  });
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  const json = await response.json();
+
+  return { props: json };
+};
